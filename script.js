43feaaa9c40f7cfa898d79bef57d7bed342abf11
luykl <leah.logsin@gmail.com>
2021-04-20 15:34:26 -0400

    added total, subtotal, tax to cart

--- script.js
+++ script.js
@@ -5,16 +5,38 @@
     let cartItemsArray = [];
     let cartItemsContainer = document.getElementById("items-in-cart");
     
+    
+        
     vendingItemsDiv.addEventListener("click", (event) => {
         if (event.target.classList.contains("item-for-sale")) {
+            
+            //update cart total
+            let subtotal = 0;
+            subtotal += Number(event.target.getAttribute("data-price"));
+            console.log(subtotal);
+
+            // add items to list
             cartItemsArray.push(event.target)
             const product = document.createElement("li");
             product.innerText = `Price: $${event.target.getAttribute("data-price")}`;
             cartItemsContainer.append(product);
-        }
-        console.log(cartItemsArray);
+
+            //update subtotal, tax and total 
+            let displaySubtotal = document.getElementById("subtotal");
+            displaySubtotal.innerText = subtotal;
+            let tax = subtotal * .06;
+            let displayTax = document.getElementById("tax");
+            displayTax.innerText = tax;
+            let total = subtotal + tax;
+            let displayTotal = document.getElementById("total");
+            displayTotal.innerText = total;
+
+            }
+        
 
     });
+
+
     
 
 
