4fc4ad9da09d5cacb0bc32b863c4537bee709a82
kaleighgriffin <kaleigh200@gmail.com>
2021-04-20 15:05:50 -0400

    pass price to cart

--- script.js
+++ script.js
@@ -3,12 +3,17 @@
     let listOfItems = document.querySelectorAll("li");
     let vendingItemsDiv = document.getElementById("vending-items")
     let cartItemsArray = [];
+    let cartItemsContainer = document.getElementById("items-in-cart");
     
     vendingItemsDiv.addEventListener("click", (event) => {
         if (event.target.classList.contains("item-for-sale")) {
             cartItemsArray.push(event.target)
+            const product = document.createElement("li");
+            product.innerText = `Price: $${event.target.getAttribute("data-price")}`;
+            cartItemsContainer.append(product);
         }
         console.log(cartItemsArray);
+
     });
     
 
