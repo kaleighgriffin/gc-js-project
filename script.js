b9eebc90d865982f022d20f1a98eaf98be3bdbcb
kaleighgriffin <kaleigh200@gmail.com>
2021-04-20 11:56:37 -0400

    demo

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+{
+    let listOfItems = document.querySelectorAll("li");
+    console.log(listOfItems[0]);
+
+    let cartItemsArray = [];
+    cartItemsArray.push(listOfItems[0]);
+    console.log(cartItemsArray);
+}
